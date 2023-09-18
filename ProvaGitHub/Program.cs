85c03904f24cd0e41ff4d412b9e77c4266a4233f
Stroppa.20126 <Stroppa.20126@LABI2-13PC18>
2023-09-18 09:42:24 +0200

    Aggiungere i file di progetto.

---
+++ ProvaGitHub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaGitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 2, 3, 4, 5, };
+        }
+    }
+}
